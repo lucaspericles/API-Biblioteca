57c76314c2f9fe934d9e47065826e03289f000ae
lucaspericles <lucaspericles.fp@gmail.com>
2022-07-05 22:44:29 -0300

    implementação dos repositórios

---
+++ src/main/java/com/campomagico/biblioteca/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.campomagico.biblioteca.repository;
+
+import com.campomagico.biblioteca.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/campomagico/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.campomagico.biblioteca.repository;
+
+import com.campomagico.biblioteca.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+}
